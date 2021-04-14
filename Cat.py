d04aedce747a063b4b980e4740b4dd44aaa2a0b2
Mighty Chen <mchen73@ucmerced.edu>
2021-04-13 21:30:31 -0700

    refactor to firebase

--- Cat.py
+++ Cat.py
@@ -1,6 +1,21 @@
 from datetime import datetime
 import time
 
+import pyrebase
+import os
+
+
+config = {
+  "apiKey": os.environ['GATHER_FB_API_KEY'],
+  "authDomain": os.environ['GATHER_FB_AUTH_DOMAIN'],
+  "databaseURL": os.environ['GATHER_FB_DATABASE_URL'],
+  "storageBucket": os.environ['GATHER_FB_STORAGE_BUCKET']
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
 SPRITE_URLS = {
     'NORMAL_URL' : "https://raw.githubusercontent.com/mchen0037/gather-bunny/master/static/stand/stand0_0.png",
     'HUNGRY_URL' : "https://raw.githubusercontent.com/mchen0037/gather-bunny/master/static/hunger/hungry_0.png",
@@ -11,15 +26,23 @@ SPRITE_URLS = {
 
 class Cat:
     def __init__(self):
-        self.name = "todo"
-        self.hunger = 100
-        self.happiness = 50
-        self.is_sleeping = False
-        self.current_sprite_url = SPRITE_URLS['NORMAL_URL']
-
-        self.level = 1
-        self.exp = 0
         self.update_state()
+
+    def reset_cat(self):
+        cat = {
+            "name": "Lily",
+            "hunger": 100,
+            "happiness": 50,
+            "is_sleeping": False,
+            "current_sprite_url" : SPRITE_URLS['NORMAL_URL'],
+            "level" : 1,
+            "exp" : 0
+        }
+
+        db.child("cats").child("Lily").set(cat)
+
+        self.update_state()
+
 
     def update_state(self):
         """
@@ -28,47 +51,86 @@ class Cat:
         # Determines if it is night time or not and returns True or False
         # Night time is determined as 10:00pm PST - 8am PST
         current_time = datetime.now()
+        new_state = self.get_state()
+
+        # self.set_sleep()
+
         if current_time.hour >= 22 or current_time.hour <= 8:
-            self.is_sleeping = True
+            # self.is_sleeping = True
+            new_state['is_sleeping'] = True
         else:
-            self.is_sleeping = False
+            new_state['is_sleeping'] = False
 
         # Reduce 1 hunger every 6 minutes--
         # 10 hunger every hour
         # 80 hunger every 8 hours
-        if current_time.minute % 6:
-            if self.is_sleeping:
-                self.hunger = max(self.hunger - 0.5, 0)
+        if current_time.minute % 6 == 0:
+            if new_state['is_sleeping']:
+                new_state['hunger'] = max(new_state['hunger'] - 0.5, 0)
             else:
-                self.hunger = max(self.hunger - 1, 0)
+                new_state['hunger'] = max(new_state['hunger'] - 1, 0)
 
-        if current_time.minute % 20:
-            if not self.is_sleeping:
-                self.happiness = self.happiness - 1
+        if current_time.minute % 20 == 0:
+            if not new_state['is_sleeping']:
+                new_state['happiness'] = new_state['happiness'] - 1
 
-        self.update_sprite()
+        new_state = self.update_sprite(new_state)
 
-    def update_sprite(self):
-        # Update Sprites
-        if self.is_sleeping:
-            self.current_sprite_url = SPRITE_URLS['SLEEPING_URL']
-        elif self.hunger < 30:
-            self.current_sprite_url = SPRITE_URLS['HUNGRY_URL']
-        elif self.happiness < 30:
-            self.current_sprite_url = SPRITE_URLS['SAD_URL']
-        elif self.happiness > 75:
-            self.current_sprite_url = SPRITE_URLS['HAPPY_URL']
+        db.child("cats").child("Lily").set(new_state)
+
+    def update_sprite(self, new_state):
+        """
+        Based on changing values of state, we need to update the sprite
+        """
+
+        if new_state['is_sleeping']:
+            new_state['current_sprite_url'] = SPRITE_URLS['SLEEPING_URL']
+        elif new_state['hunger'] < 30:
+            new_state['current_sprite_url'] = SPRITE_URLS['HUNGRY_URL']
+        elif new_state['happiness'] < 30:
+            new_state['current_sprite_url'] = SPRITE_URLS['SAD_URL']
+        elif new_state['happiness'] > 75:
+            new_state['current_sprite_url'] = SPRITE_URLS['HAPPY_URL']
         else:
-            self.current_sprite_url = SPRITE_URLS['NORMAL_URL']
+            new_state['current_sprite_url'] = SPRITE_URLS['NORMAL_URL']
+        return new_state
+
 
     def play(self):
-        delta = min(self.happiness + 20, 100) - self.happiness
-        self.happiness = self.happiness + delta
-        self.exp = self.exp + (1 / self.level * delta)
-        self.update_sprite()
+        """
+        Grab the state from the Firebase DB and then update happiness
+        """
+
+        new_state = self.get_state()
+        delta = min(new_state['happiness'] + 20, 100) - new_state['happiness']
+        new_state['happiness'] = new_state['happiness'] + delta
+        new_state['exp'] = new_state['exp'] + (1 / new_state['level'] * delta)
+
+        # Need to update the sprite if necessary
+        new_state = self.update_sprite(new_state)
+        db.child("cats").child("Lily").set(new_state)
 
     def feed(self):
-        delta = min(self.hunger + 20, 100) - self.hunger
-        self.hunger = self.hunger + delta
-        self.exp = self.exp + (1 / self.level * delta)
-        self.update_sprite()
+        """
+        Grab the state from the Firebase DB and then update hunger
+        """
+
+        new_state = self.get_state()
+        delta = min(new_state['hunger'] + 20, 100) - new_state['hunger']
+        new_state['hunger'] = new_state['hunger'] + delta
+        new_state['exp'] = new_state['exp'] + (1 / new_state['level'] * delta)
+
+        # Need to update the sprite if necessary
+        new_state = self.update_sprite(new_state)
+        db.child("cats").child("Lily").set(new_state)
+
+    def test(self):
+        """
+        Lower exp, happiness, and hunger to test the cat
+        """
+        db.child("cats").child("Lily").child("happiness").set(30)
+        db.child("cats").child("Lily").child("hunger").set(30)
+        # db.child("cats").child("Lily").child("is_sleeping").set(True)
+
+    def get_state(self):
+        return db.child("cats").child("Lily").get().val()
--- server.py
+++ server.py
@@ -12,6 +12,8 @@ import time
 import threading
 import jsonify
 
+import pyrebase
+
 from Cat import Cat
 
 app = Flask(__name__)
@@ -20,38 +22,41 @@ if 'DYNO' in os.environ:
     sslify = SSLify(app)
 
 c = Cat()
-c.name = "Lily"
 
-lock_variables = False
+config = {
+  "apiKey": os.environ['GATHER_FB_API_KEY'],
+  "authDomain": os.environ['GATHER_FB_AUTH_DOMAIN'],
+  "databaseURL": os.environ['GATHER_FB_DATABASE_URL'],
+  "storageBucket": os.environ['GATHER_FB_STORAGE_BUCKET']
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
 
 @app.before_first_request
 def light_thread():
     def run():
         while True:
-            global c
-            global lock_variables
-            lock_variables = True
             c.update_state()
-            lock_variables = False
-            time.sleep(65)
+            time.sleep(60)
     thread = threading.Thread(target=run)
     thread.start()
 
 
 @app.route("/")
 def cat():
-    print("hunger", c.hunger)
-    print("happiness", c.happiness)
+    state = c.get_state()
     return render_template(
         'index.html',
         title="Hello",
-        pet_sprite=c.current_sprite_url,
-        pet_name=c.name,
-        pet_hunger=int(c.hunger),
-        pet_happiness=int(c.happiness),
-        pet_is_sleeping=c.is_sleeping,
-        pet_level=int(c.level),
-        pet_exp=int(c.exp)
+        pet_sprite=state['current_sprite_url'],
+        pet_name=state['name'],
+        pet_hunger=int(state['hunger']),
+        pet_happiness=int(state['happiness']),
+        pet_is_sleeping=state['is_sleeping'],
+        pet_level=int(state['level']),
+        pet_exp=int(state['exp'])
     )
 
 @app.route("/feed")
